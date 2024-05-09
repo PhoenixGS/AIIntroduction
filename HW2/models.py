d0847b13bb5a4f76496c3a7bc333be1cd6b4c8cf
PhoenixGS <thestarrydream@gmail.com>
2024-05-09 20:28:45 +0800

    update HW2
    update main.py & models.py

--- HW2/models.py
+++ HW2/models.py
@@ -36,15 +36,24 @@ class MLP2(torch.nn.Module):
 class CNN(torch.nn.Module):
     def __init__(self, embed_dim=50, filter_sizes=[3, 4, 5], num_filters=[10, 10, 10], num_classes=2, dropout=0.5):
         super(CNN, self).__init__()
-        self.convs = torch.nn.ModuleList([torch.nn.Conv1d(in_channels=embed_dim, out_channels=num_filters[i], kernel_size=filter_sizes[i]) for i in range(len(filter_sizes))])
+        # self.convs = torch.nn.ModuleList([torch.nn.Conv1d(in_channels=embed_dim, out_channels=num_filters[i], kernel_size=filter_sizes[i]) for i in range(len(filter_sizes))])
+        self.conv1 = torch.nn.Conv1d(in_channels=embed_dim, out_channels=num_filters[0], kernel_size=filter_sizes[0])
+        self.conv2 = torch.nn.Conv1d(in_channels=embed_dim, out_channels=num_filters[1], kernel_size=filter_sizes[1])
+        self.conv3 = torch.nn.Conv1d(in_channels=embed_dim, out_channels=num_filters[2], kernel_size=filter_sizes[2])
         self.dropout = torch.nn.Dropout(dropout)
         self.fc = torch.nn.Linear(sum(num_filters), num_classes)
 
     def forward(self, x):
         x = x.permute(0, 2, 1)
-        x = [torch.nn.functional.relu(conv(x)) for conv in self.convs]
-        x = [torch.nn.functional.max_pool1d(i, i.size(2)).squeeze(2) for i in x]
-        x = torch.cat(x, 1)
+        # x = [torch.nn.functional.relu(conv(x)) for conv in self.convs]
+        # x = [torch.nn.functional.max_pool1d(i, i.size(2)).squeeze(2) for i in x]
+        x1 = torch.nn.functional.relu(self.conv1(x))
+        x2 = torch.nn.functional.relu(self.conv2(x))
+        x3 = torch.nn.functional.relu(self.conv3(x))
+        x1 = torch.nn.functional.max_pool1d(x1, x1.size(2)).squeeze(2)
+        x2 = torch.nn.functional.max_pool1d(x2, x2.size(2)).squeeze(2)
+        x3 = torch.nn.functional.max_pool1d(x3, x3.size(2)).squeeze(2)
+        x = torch.cat([x1, x2, x3], 1)
         x = self.dropout(x)
         x = self.fc(x)
         return x
@@ -54,10 +63,14 @@ class RNN(torch.nn.Module):
         super(RNN, self).__init__()
         self.lstm = torch.nn.LSTM(embed_dim, hidden_dim, num_layers, batch_first=True, dropout=dropout)
         self.fc = torch.nn.Linear(hidden_dim, num_classes)
+        # self.fc1 = torch.nn.Linear(hidden_dim, 50)
+        # self.fc2 = torch.nn.Linear(50, num_classes)
 
     def forward(self, x):
         _, (h_n, _) = self.lstm(x)
         x = self.fc(h_n[-1])
+        # x = torch.nn.functional.relu(self.fc1(h_n[-1]))
+        # x = self.fc2(x)
         return x
 
 class RNN2(torch.nn.Module):
@@ -71,3 +84,4 @@ class RNN2(torch.nn.Module):
         x = torch.cat((h_n[-2], h_n[-1]), dim=1)
         x = self.fc(x)
         return x
+
